792ab48380d542b838137374566fe19605cae69f
Sudhanshu Kumar <114154455+Sudhanshu-06@users.noreply.github.com>
2024-03-05 11:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function validEmail(str) {
-  //your JS code here.
-}
+function validEmail(email) {
+ 
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-// Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+  return emailPattern.test(email);
+}
